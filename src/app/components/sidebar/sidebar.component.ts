a3995dd245d13ddebed4141ba3e336d8e0b47dec
Ria Pacheco <ria@mydomastudio.com>
2022-05-25 11:00:47 -0600

    Added notes to SCSS file

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,12 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
+  animations: [
+    trigger('sidebarTrigger', [
+      // To add a cool "enter" animation for the sidebar
+      transition(':enter', [
+        style({ transform: 'translateX(-100%)' }),
+        animate('300ms ease-in', style({ transform: 'translateY(0%)' }))
+      ]),
+
+      // To define animations based on trigger actions
+      state('open', style({ transform: 'translateX(0%)' })),
+      state('close', style({ transform: 'translateX(-92%)' })),
+      transition('open => close', [
+        animate('300ms ease-in')
+      ]),
+      transition('close => open', [
+        animate('300ms ease-out')
+      ])
+    ])
+  ]
 })
 export class SidebarComponent implements OnInit {
-  showsSidebar = false;
+  showsSidebar = true;
   sections = [
     {
       sectionHeading: 'The Evolving State of SE',
@@ -67,8 +87,13 @@ export class SidebarComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  private closeSidebar() { this.showsSidebar = false; }
+
   onTargetContentClick(targetString: string, event: Event) {
     console.log(targetString);
+    this.closeSidebar();
   }
+
+
 
 }
